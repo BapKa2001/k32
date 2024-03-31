616c384f023893d4bddb7de32cc68e0fcd6686b8
Nguyen Huu Thien <thienhuunguyen42@gmail.com>
2024-03-31 11:48:17 +0700

    k32

---
+++ src/k32.java
@@ -0,0 +1,7 @@
+public class k32 {
+    public static void main(String[] args) {
+        System.out.println("k32 buổi 1");
+        System.out.println("k32 buổi 2");
+        System.out.println("k32 code ");
+    }
+}
